66b4e3138aadbad9aaa5c7cfe84ccc3d0f791b98
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2018-12-01 14:06:42 +0100

    Added ennemie prototype

---
+++ src/Ennemie.ts
@@ -0,0 +1,22 @@
+export default class Ennemie {
+  private scene: Phaser.Scene;
+  private ennemieObj: any;
+  constructor(
+    scene: Phaser.Scene,
+    player: Phaser.Physics.Arcade.Sprite,
+    x?: number,
+    y?: number
+  ) {
+    if (!x) {
+      x = 0;
+    }
+    if (!y) {
+      y = 0;
+    }
+    this.scene = scene;
+    this.ennemieObj = scene.physics.add.sprite(x, y, "ennemie");
+  }
+  refreshAttack(player: Phaser.Physics.Arcade.Sprite) {
+    this.scene.physics.moveToObject(this.ennemieObj, player, 50);
+  }
+}
