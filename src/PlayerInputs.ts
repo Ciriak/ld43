c253d5dabd84c0681efaa366be5eec882fcb1aac
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2018-12-01 12:17:31 +0100

    Added playerInput class

---
+++ src/PlayerInputs.ts
@@ -0,0 +1,17 @@
+export default class PlayerInputs {
+  update(time: number, delta: number, player: any, scene: any) {
+    if (scene.cursors.left.isDown) {
+      player.x -= 5;
+      console.log(player);
+    }
+    if (scene.cursors.right.isDown) {
+      player.x += 5;
+    }
+    if (scene.cursors.down.isDown) {
+      player.y += 5;
+    }
+    if (scene.cursors.up.isDown) {
+      player.y -= 5;
+    }
+  }
+}
--- src/scenes/Scene.ts
+++ src/scenes/Scene.ts
@@ -1,6 +1,7 @@
 let player: any;
 import DungeonMap from "../dungeon/DungeonMap";
-
+import PlayerInputs from "../PlayerInputs";
+let playerInputs = new PlayerInputs();
 class MainScene extends Phaser.Scene {
   gridUnit: 8;
   cursors: any;
@@ -39,22 +40,7 @@ class MainScene extends Phaser.Scene {
   }
 
   update(time: number, delta: number) {
-    if (this.cursors.left.isDown) {
-      player.x -= 5;
-    }
-    if (this.cursors.right.isDown) {
-      player.x += 5;
-    }
-    if (this.cursors.down.isDown) {
-      player.y += 5;
-    }
-    if (this.cursors.up.isDown) {
-      player.y -= 5;
-    }
-    this.generate();
-    this.setGroundCollider();
-
-    this.physics.add.collider(player, this.groundCollider, null, null, null);
+    playerInputs.update(time, delta, player, this);
   }
 
   /**
@@ -73,73 +59,6 @@ class MainScene extends Phaser.Scene {
     });
 
     const tileset = this.map.addTilesetImage("tiles", null, 48, 48, 1, 2);
-
-    this.groundLayer = this.map.createBlankDynamicLayer(
-      "layer",
-      tileset,
-      null,
-      null,
-      null,
-      null
-    );
-    console.log(this.groundLayer);
-  }
-
-  /**
-   * Generate blocs when camera move
-   */
-
-  generate() {
-    if (!this.map) {
-      return;
-    }
-
-    // Turn the dungeon into a 2D array of tiles where each of the four types of tiles is mapped to a
-
-    // tile index within our tileset. Note: using -1 for empty tiles means they won't render.
-
-    const mappedTiles = this.DungeonRandom.getMapTiles({
-      empty: -1,
-      floor: 6,
-      door: 6,
-      wall: 20
-    });
-    const camera = this.cameras.main;
-    let coordinates = {
-      x: camera.width + camera.x,
-      y: camera.height + camera.y
-    };
-
-    //update the values
-    while (this.maxCoordinates.x < coordinates.x) {
-      const tile = this.groundLayer.putTileAt(
-        mappedTiles,
-        this.maxCoordinates.x,
-        window.innerHeight + 58
-      );
-      this.maxCoordinates.x++;
-
-      // this.physics.world.setBounds(
-      //   0,
-      //   window.innerHeight,
-      //   this.maxCoordinates.x + 50,
-      //   500
-      // );
-    }
-  }
-
-  /**
-   * update ground collider
-   */
-  setGroundCollider() {
-    if (!this.groundCollider) {
-      this.groundCollider = this.physics.add.sprite(0, 0, "groundCollider");
-
-      this.groundCollider.collideWorldBounds = true;
-      this.groundCollider.allowGravity = false;
-    }
-    this.groundCollider.x = player.x;
-    this.groundCollider.y = window.innerHeight + 50;
   }
 }
 
