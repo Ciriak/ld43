0c66d9c2a72ee9da6e3a0cb9dde665f883446b2c
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2018-12-01 16:07:28 +0100

    added web UI

--- src/UIManager.ts
+++ src/UIManager.ts
@@ -1,6 +1,15 @@
 import * as $ from "jquery";
 export default class UIManager {
+  private scene: Phaser.Scene;
+  itemsCard = {
+    hand: null,
+    foot: null,
+    eye: null,
+    hearth: null
+  };
   constructor(scene: Phaser.Scene) {
+    this.scene = scene;
+
     this.generateItemsCards();
     $(".play-btn").click(function() {
       $("#main-menu").fadeOut();
